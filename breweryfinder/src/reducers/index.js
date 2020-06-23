47d044972a6a78a7ec231cecf5268c6ed06f2d47
Chris Forrest <redrider_cr@yahoo.com>
2020-06-23 11:44:54 -0400

    made initial empty reducer made store and wrapped Provider around app

---
+++ breweryfinder/src/reducers/index.js
@@ -0,0 +1,8 @@
+/***********************made initial state to put in the store ********************************************/
+
+export const appReducer = (state, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}

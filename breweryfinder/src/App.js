38e25ee4a9b51ecf4f8b249f4f2d8856adce8bd4
Chris Forrest <redrider_cr@yahoo.com>
2020-06-23 11:21:23 -0400

    downloaded dependencies changed the name displayed in app

---
+++ breweryfinder/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Brewery Finder</h1>
+     <h4>powered by Open Brewer DB</h4>
+    </div>
+  );
+}
+
+export default App;

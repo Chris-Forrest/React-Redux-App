9372e90d341d2d674efbf44b872f5a4dd31ee51b
Chris Forrest <redrider_cr@yahoo.com>
2020-06-23 13:30:56 -0400

    made action and reducers and initialState

--- breweryfinder/src/actions/index.js
+++ breweryfinder/src/actions/index.js
@@ -1,3 +1,17 @@
+import axios from 'axios';
+
 export const FETCHING_BREWERIES_START = "FETCHING_BREWERIES_START";
 export const FETCHING_BREWERIES_SUCCESS = "FETCHING_BREWERIES_SUCCESS";
 export const FETCHING_BREWERIES_FAILURE = "FETCHING_BREWERIES_FAILURE";
+
+
+export const getBreweries = () => dispatch => {
+    dispatch({ type: FETCHING_BREWERIES_START });
+    axios
+     .get("https://api.openbrewerydb.org/breweries?by_state=pennsylvania")
+     .then( res => {
+         dispatch({ type: FETCHING_BREWERIES_SUCCESS, payload: res.data})
+         console.log('data from the axios call ',res.data)
+     })
+     .catch( err => ('error in actions', err))
+}
--- breweryfinder/src/reducers/index.js
+++ breweryfinder/src/reducers/index.js
@@ -1,7 +1,34 @@
-/***********************made initial state to put in the store ********************************************/
+/*************imports from actions **********************************************************************/
+import { FETCHING_BREWERIES_START, FETCHING_BREWERIES_SUCCESS, FETCHING_BREWERIES_FAILURE} from '../actions';
 
-export const appReducer = (state, action) => {
+/***********************made initial state ********************************************/
+const initialState = {
+    isFetching : false,
+    breweries:[],
+    error:""
+}
+/****************app reducer  ******************************************************/
+
+export const appReducer = ( state = initialState, action) => {
     switch(action.type){
+        case FETCHING_BREWERIES_START:
+            return {
+                ...state,
+                isFetching: true,
+                error: ""
+            };
+        case FETCHING_BREWERIES_SUCCESS:
+            return {
+                ...state,
+                breweries: action.payload,
+                isFetching: false
+            }
+        case FETCHING_BREWERIES_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error:"error retrieving breweries"
+            }
         default:
             return state;
     }

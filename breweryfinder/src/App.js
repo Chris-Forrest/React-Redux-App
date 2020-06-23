ee1b321863206ef7e3e64159c1277f93fa2a6e03
Chris Forrest <redrider_cr@yahoo.com>
2020-06-23 15:58:24 -0400

    app working and styled mvp met

--- breweryfinder/src/App.js
+++ breweryfinder/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
-import Breweries from './components/Breweries'
+import Breweries from './components/Breweries';
+import './images/image1.png';
+import styled from 'styled-components';
+import './images/image2.png'
+
+const StyledImage = styled.img `
+width:30%;
+height:30%;
+`
+const CraftImage = styled.img `
+width:30%;
+height:30%
+`
 
 function App() {
   return (
     <div className="App">
+      <CraftImage src={require('./images/image2.png')} alt="neon sign craft on rooftop" />
+      <StyledImage src={ require('./images/image1.png')} alt="neon sign beer on rooftop"/>
      <h1>Brewery Finder</h1>
      <h4>powered by Open Brewer DB</h4>
      <Breweries />
--- breweryfinder/src/components/Brewery.js
+++ breweryfinder/src/components/Brewery.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledBrewery = styled.div`
+border:1px solid rgb(210, 210, 210 );
+border-radius: 5px;
+box-shadow: 10px 8px 12px -2px rgb(128, 127, 197);
+margin: 8px;
+padding: 12px;
+background-color:#076db5;
+width: 50%;
+margin-left: 25%;
+color: #044008;
+`
 
 
 const Brewery = ({ brewery }) => {
 
     return (
-        <div className='brewery'>
+        <StyledBrewery className='brewery'>
             <h3>Brewery :{brewery.name}</h3>
             <h4>Brewery Type :{brewery.brewery_type}</h4>
             <h4>Address :{brewery.street} </h4>
@@ -12,7 +25,7 @@ const Brewery = ({ brewery }) => {
             <h4>State :{brewery.state} </h4>
             <h4>Phone Number : {brewery.phone} </h4>
             <a href={brewery.website_url}>webisite :{brewery.website_url} </a>
-        </div>
+        </StyledBrewery>
     )
 }
 
